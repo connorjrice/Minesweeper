c8397862ace3f8053b7929184be7b118ca50b4cb
Connor Rice <connor.rice@coloradocollege.edu>
2014-12-31 16:46:37 -0600

    Initial

---
+++ desktop/src/com/classics/minesweeper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.classics.minesweeper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.classics.minesweeper.MinesweeperMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MinesweeperMain(), config);
+	}
+}
